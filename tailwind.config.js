7e1d115a621d44339c453fc8d9aa7be30d0e46bb
iinaamasum <iinaamasum@gmail.com>
2022-07-11 16:32:04 +0600

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#6E0B75',
+          secondary: '#007EBD',
+          accent: '#22c55e',
+          neutral: '#1F2937',
+          'base-100': '#FFFFFF',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+      'fantasy',
+    ],
+  },
+};
