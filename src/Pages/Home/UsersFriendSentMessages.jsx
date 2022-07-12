3c11b95e9e0990daffc5467d11536d996650656b
iinaamasum <iinaamasum@gmail.com>
2022-07-12 23:53:06 +0600

    user sent messages card done with friend messages card fix

--- src/Pages/Home/UsersFriendSentMessages.jsx
+++ src/Pages/Home/UsersFriendSentMessages.jsx
@@ -6,17 +6,17 @@ const UsersFriendSentMessages = () => {
   return (
     <section className="p-4 w-full">
       <div className="flex items-end w-full">
-        <img className="w-5 h-5" src={userFriend} alt="" />
-        <p className="bg-gray-200 rounded-xl w-[70%] p-2 text-sm ml-2">
+        <img className="w-5 h-5 rounded-full" src={userFriend} alt="" />
+        <p className="bg-gray-200 rounded-xl max-w-[70%] p-2 text-sm ml-2">
           message - Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Excepturi distinctio placeat rem quia laboriosam officiis nemo quos
           quaerat delectus deleniti.
         </p>
+        <BsThreeDots className="ml-3" />
       </div>
-      <div className="flex ml-8 w-[70%] items-center justify-between">
-        <p className="w-20 font-sans text-sm">9:20 PM</p>
-        <BsThreeDots className="mr-3" />
-      </div>
+      <p className="w-20 font-sans text-[13px] font-medium text-gray-400 ml-8">
+        9:20 PM
+      </p>
     </section>
   );
 };
--- src/Pages/Home/UsersSentMessageCard.jsx
+++ src/Pages/Home/UsersSentMessageCard.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+import userFriend from '../../images/logo/logo.png';
 
 const UsersSentMessageCard = () => {
-  return <div></div>;
+  return (
+    <section className="p-4 w-full">
+      <div className="flex justify-end items-end w-full">
+        <BsThreeDots className="mr-3" />
+        <p className="bg-red-400 text-white rounded-xl max-w-[70%] p-2 text-sm mr-2">
+          message - Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Excepturi distinctio placeat rem quia laboriosam officiis nemo quos
+          quaerat delectus deleniti.
+        </p>
+        <img className="w-5 h-5 rounded-full" src={userFriend} alt="" />
+      </div>
+      <div className="flex w-full justify-end">
+        <p className="w-20 font-sans text-[13px] font-medium text-gray-400 mr-2">
+          9:20 PM
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default UsersSentMessageCard;
